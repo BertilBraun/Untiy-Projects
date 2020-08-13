fd85f7032cc053de80922c471e2892f555f99d60
OrtwIn <bertil.braun.private@gmail.com>
2020-08-13 15:07:16 +0200

    Added Builds

--- Ray Tracing/Assets/ComputeController.cs
+++ Ray Tracing/Assets/ComputeController.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
 
-namespace Assets
-{
-    [CustomEditor(typeof(ComputeProgram))]
-    class ComputeController : Editor
-    {
-        public override void OnInspectorGUI()
-        {
-            ComputeProgram program = target as ComputeProgram;
-            GUILayout.Label("Raytracing Controller");
+//namespace Assets
+//{
+//    [CustomEditor(typeof(ComputeProgram))]
+//    class ComputeController : Editor
+//    {
+//        public override void OnInspectorGUI()
+//        {
+//            ComputeProgram program = target as ComputeProgram;
+//            GUILayout.Label("Raytracing Controller");
 
-            if (GUILayout.Button("Regenerate"))
-                program.SetUpScene();
+//            if (GUILayout.Button("Regenerate"))
+//                program.SetUpScene();
 
-            program.reflectionCount = EditorGUILayout.IntSlider("Reflection Count", program.reflectionCount, 1, 8);
-            program.SphereCount = EditorGUILayout.IntSlider("Sphere Count", program.SphereCount / 100, 1, 4) * 100;
-        }
-    }
-}
+//            program.reflectionCount = EditorGUILayout.IntSlider("Reflection Count", program.reflectionCount, 1, 8);
+//            program.SphereCount = EditorGUILayout.IntSlider("Sphere Count", program.SphereCount / 100, 1, 4) * 100;
+//        }
+//    }
+//}
