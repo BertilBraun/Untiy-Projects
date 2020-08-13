1788fc452e3ff465c6e8d9e54aed6affaea115e7
OrtwIn <bertil.braun.private@gmail.com>
2020-08-13 14:22:34 +0200

    Basic RayMarching
    Basic Ray Marching implementation

---
+++ Ray Marching/Assets/CameraInteraction.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class CameraInteraction : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+    public float speed = 10.0f;
+    private float translation;
+    private float straffe;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = (Cursor.lockState == CursorLockMode.Locked) ? CursorLockMode.None : CursorLockMode.Locked;
+
+        if (Cursor.lockState != CursorLockMode.Locked)
+            return;
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+
+        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            translation *= 15f;
+            straffe *= 15f;
+        }
+
+        transform.Translate(straffe, 0, translation);
+    }
+}
--- Ray Marching/Assets/ComputeProgram.cs
+++ Ray Marching/Assets/ComputeProgram.cs
@@ -1,26 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComputeProgram : MonoBehaviour
 {
-    public ComputeShader compute;
-    public RenderTexture result;
+    public Image image;
 
-    public Color color = Color.red;
+    public ComputeShader compute;
+    public Texture skyboxTexture;
+    public Light directionalLight;
+
+    public int reflectionCount = 4;
+
+    private RenderTexture result;
+    private new Camera camera;
 
     void Start()
     {
-        result.Release();
-        result.enableRandomWrite = true;
-        result.Create();
+        camera = Camera.main;
+
+        InitTexture();
     }
 
     void Update()
     {
         var kernel = compute.FindKernel("CSMain");
+
+        Vector3 l = directionalLight.transform.forward;
+        compute.SetVector("DirectionalLight", new Vector4(l.x, l.y, l.z, directionalLight.intensity));
+
+        compute.SetFloat("iTime", Time.time);
+        compute.SetInt("ReflectionCount", reflectionCount);
+        compute.SetTexture(kernel, "SkyboxTexture", skyboxTexture);
         compute.SetTexture(kernel, "Result", result);
-        compute.SetVector("Color", color);
-        compute.Dispatch(kernel, result.width / 8, result.height / 8, 1);
+        compute.SetMatrix("CameraToWorld", camera.cameraToWorldMatrix);
+        compute.SetMatrix("CameraInverseProjection", camera.projectionMatrix.inverse);
+
+        int threadGroupsX = Mathf.CeilToInt(result.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(result.height / 8.0f);
+        compute.Dispatch(kernel, threadGroupsX, threadGroupsY, 1);
+    }
+
+    void InitTexture()
+    {
+        result = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        result.enableRandomWrite = true;
+        result.Create();
+        image.material.mainTexture = result;
     }
 }
